956fb859ff3be075ae01e1815e4913c1eafdcbe4
mgamatero <mgamatero@yahoo.com>
2018-10-04 23:50:30 -0700

    .gitignore, bootstrap added, headers added via partials

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express')
+const hbs = require('hbs')
 var app = express()
 
+
+//middleware
 app.set(express.static(__dirname + '/public'))
 
-app.get('/',(req,res)=>{
-    res.send('Hello Mundo')
+//middleware for handlebars
+app.set('view engine','hbs')
+
+//middleware for partials
+hbs.registerPartials(__dirname + '/views/partials')
+
+//routes
+app.get('/home',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home',
+        content:'homehomehomehomehome'
+    })
+})
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About',
+        content:'aboutaboutaboutaboutabout'
+    })
+})
+
+
+app.get('/contact',(req,res)=>{
+    res.render('contact.hbs',{
+        pageTitle:'Contact',
+        content:'contactcontactcontactcontact'
+    })
 })
 
 app.listen(3000,() =>{
